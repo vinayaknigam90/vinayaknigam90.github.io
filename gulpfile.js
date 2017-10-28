fd67e57e8efa7cef6130ae6e21eb463c3e6c3dd1
vinayaknigam90 <vinayak.nigam@sjsu.edu>
2017-10-27 18:05:56 -0700

    new theme boot

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
 var pkg = require('./package.json');
 
 // Set the banner content
@@ -19,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/freelancer.scss')
+  return gulp.src('scss/grayscale.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
@@ -32,7 +31,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/freelancer.css')
+  return gulp.src('css/grayscale.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
     }))
@@ -47,7 +46,7 @@ gulp.task('minify-css', ['sass'], function() {
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
+  return gulp.src('js/grayscale.js')
     .pipe(uglify())
     .pipe(header(banner, {
       pkg: pkg
